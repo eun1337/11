18aa475ddf5092d13b88e6fa5c09c6338f368424
eun1337 <dbdmstj0327@naver.com>
2023-11-16 16:06:46 +0900

    05

--- main.c
+++ main.c
@@ -2,22 +2,21 @@
 
 int main()
 {
-	int i ;
-	int grade[5];
-	
-	for(i = 0; i < 5; i++) {
-		printf("input value [%i] = ", i);
-		scanf("%d", &grade[i]);
-	}
-	
-	int sum = 0;
-	
-	for(i = 0; i < 5; i++) {
-		printf("grade[%i] = %i\n", i, *(grade+i));
-		sum+= *(grade+i);
-	}
-	
-	printf("average : %i", sum/5);
-	
-	return 0;
+   char *pc;
+   int *pi;
+   double *pd;
+   
+   pc = (char *)10000;
+   pi = (int*)10000;
+   pd = (double *)10000;
+   
+   printf("증가 전 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
+   
+   pc++;
+   pi++;
+   pd++;
+   printf("증가 후 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
+
+   system("PAUSE");
+   return 0;
 }
